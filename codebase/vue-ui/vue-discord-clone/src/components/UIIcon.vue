cadfbbb26a8a66df52f6b3ade8b3ad83f9af43a5
awun0105 <anhquanlam2002@gmail.com>
2026-09-18 11:27:19 +0700

    chore: remove duplicate inner folder in vue-ui

--- codebase/vue-ui/vue-discord-clone/src/components/UIIcon.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { Icons, type IconType } from '@/Icons';
-
-const props = defineProps<{ icon: IconType }>();
-</script>
-
-<template>
-  <component v-if="props.icon" :is="Icons.get(props.icon)"></component>
-</template>
